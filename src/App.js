8b825935f3857aacd002b63b3e5d5380fab3fddd
brettsodie <blsodie@gmail.com>
2021-02-28 12:49:11 -0500

    feat: tex change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SCU
+          another change
         </p>
         <a
           className="App-link"
